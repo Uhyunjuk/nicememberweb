e1b94d1aa7b7b820247900c5d8721479ca4fb40e
juk <119032872+Uhyunjuk@users.noreply.github.com>
2023-01-26 17:41:37 +0900

    Add files via upload

--- NiceMemberWeb/src/notice/model/dao/NoticeDAO.java
+++ NiceMemberWeb/src/notice/model/dao/NoticeDAO.java
@@ -12,6 +12,13 @@ import notice.model.vo.Notice;
 
 public class NoticeDAO {
 	
+	
+	/**
+	 * 공지사항 등록 DAO
+	 * @param conn
+	 * @param notice
+	 * @return result
+	 */
 	public int insertNotice(Connection conn, Notice notice) {
 		String query = "INSERT INTO NOTICE_TBL VALUES(SEQ_NOTICENO.NEXTVAL,?,?,?,DEFAULT,DEFAULT)";
 		int result = 0;
@@ -29,6 +36,11 @@ public class NoticeDAO {
 		return result;
 	}
 
+	/**
+	 * 공지사항 목록 조회 DAO
+	 * @param conn
+	 * @return nList
+	 */
 	public List<Notice> selectAllNotice(Connection conn) {
 		String query = "SELECT * FROM NOTICE_TBL";
 		List<Notice> nList = null;
@@ -51,5 +63,57 @@ public class NoticeDAO {
 			e.printStackTrace();
 		}
 		return nList;
+	}
+
+	/**
+	 * 공지사항 상세조회 DAO
+	 * @param conn
+	 * @param noticeNo
+	 * @return notice
+	 */
+	public Notice selectOnebyNo(Connection conn, int noticeNo) {
+		String query = "SELECT * FROM NOTICE_TBL WHERE NOTICE_NO = ?";
+		Notice notice = null;
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, noticeNo);
+			ResultSet rset = pstmt.executeQuery();
+			// 후처리 (rset -> notice)
+			if(rset.next()) {
+				notice = new Notice();
+				notice.setNoticeNo(rset.getInt("NOTICE_NO"));
+				notice.setNoticeSubject(rset.getString("NOTICE_SUBJECT"));
+				notice.setNoticeContent(rset.getString("NOTICE_CONTENT"));
+				notice.setNoticeWriter(rset.getString("NOTICE_WRITER"));
+				notice.setNoticeDate(rset.getTimestamp("NOTICE_DATE"));
+				notice.setNoticeCount(rset.getInt("NOTICE_COUNT"));
+			}
+			rset.close();
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return notice;
+	}
+
+	/**
+	 * 공지사항 삭제 DAO
+	 * @param conn
+	 * @param noticeNo
+	 * @return result
+	 */
+	public int deleteNotcie(Connection conn, int noticeNo) {
+		String query = "DELETE FROM NOTICE_TBL WHERE NOTICE_NO = ?";
+		int result = 0;
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, noticeNo);
+			result = pstmt.executeUpdate();
+			
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
