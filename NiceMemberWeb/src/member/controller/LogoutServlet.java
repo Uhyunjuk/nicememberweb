ebd369d0671fc9401292728cbae45f299f510664
juk <119032872+Uhyunjuk@users.noreply.github.com>
2023-01-26 17:43:04 +0900

    Add files via upload

--- NiceMemberWeb/src/member/controller/LogoutServlet.java
+++ NiceMemberWeb/src/member/controller/LogoutServlet.java
@@ -14,18 +14,17 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/member/logout.kh")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public LogoutServlet() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
--- NiceMemberWeb/src/member/controller/UpdateServlet.java
+++ NiceMemberWeb/src/member/controller/UpdateServlet.java
@@ -30,25 +30,25 @@ public class UpdateServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 수정가능 :  아이디,비밀번호, 이메일, 주소, 폰, 취미
-		request.setCharacterEncoding("UTF-8");
-		String memeberId = request.getParameter("member-id");
-		String memberPw = request.getParameter("member-pw");
-		String memberEmail = request.getParameter("member-email");
-		String memberAddress = request.getParameter("member-address");
-		String memberPhone = request.getParameter("member-phone");
-		String memberHobby = request.getParameter("member-hobby");
-		Member member = new Member(memeberId, memberPw, memberEmail, memberAddress, memberPhone, memberHobby);
-		MemberService mService = new MemberService();
-		int result = mService.updateMember(member);
-		if(result > 0) {
-			// 성공하면 메인페이지
-			response.sendRedirect("/index.jsp");
-		}else {
-			// 실패하면 에러페이지
-			request.setAttribute("title", "정보 수정 실패");
-			request.setAttribute("msg", "회원 정보 수정이 완료되지 않았습니다.");
-			request.getRequestDispatcher("/WEB-INF/views/member/error.jsp").forward(request, response);
-		}
+				request.setCharacterEncoding("UTF-8");
+				String memeberId = request.getParameter("member-id");
+				String memberPw = request.getParameter("member-pw");
+				String memberEmail = request.getParameter("member-email");
+				String memberAddress = request.getParameter("member-address");
+				String memberPhone = request.getParameter("member-phone");
+				String memberHobby = request.getParameter("member-hobby");
+				Member member = new Member(memeberId, memberPw, memberEmail, memberAddress, memberPhone, memberHobby);
+				MemberService mService = new MemberService();
+				int result = mService.updateMember(member);
+				if(result > 0) {
+					// 성공하면 메인페이지
+					response.sendRedirect("/index.jsp");
+				}else {
+					// 실패하면 에러페이지
+					request.setAttribute("title", "정보 수정 실패");
+					request.setAttribute("msg", "회원 정보 수정이 완료되지 않았습니다.");
+					request.getRequestDispatcher("/WEB-INF/views/member/error.jsp").forward(request, response);
+				}
 	}
 
 }
